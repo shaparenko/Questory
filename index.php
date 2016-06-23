aaea56aa18e28c64a344d6c98ff66c5ab052fe8b
gmskov <gms.kov@gmail.com>
2016-06-23 20:09:03 +0300

    23.06.2016

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 define ("O_PATH", dirname(__FILE__));
 include O_PATH.'/app/bootstrap.php';
+
+/*
+
+ #getCodeMod{
+    max-height: 38em;
+    min-height: 1em;
+}
+.popup-info-i-text{
+    //max-height: calc(100vh - 210px);
+overflow-y: auto;
+    max-height: 38em;
+    min-height: 1em;
+    margin: 3em 0 3em 2em;
+    padding-right: 3em;
+    font-size: 14px;
+    line-height: 20px;
+}
+  
+ */
